1b1434dabc742454fc0e77816a61e27cbc491a75
Ling Dev <98570765+lingneisandev@users.noreply.github.com>
2026-08-18 12:03:15 +0700

    Translate comments to Indonesian in musicButtons.js
    Updated comments in musicButtons.js to use Indonesian language.

--- src/Interactions/musicButtons.js
+++ src/Interactions/musicButtons.js
@@ -6,11 +6,11 @@ module.exports = async (client, interaction) => {
   const allowedCustomIds = ["play_now", "upcoming", "remove_song"];
   if (!allowedCustomIds.includes(interaction.customId)) return;
 
-  // Ye interaction `play.js` ke collector se handle hota hai,
-  // isliye yahan kuch reply/update nahi karna hai
+  // Interaksi `play.js` Hal itu ditangani oleh petugas penagih,
+  // Jadi, inilah beberapa di antaranya reply/update Saya tidak ingin melakukannya
 
-  // But agar interaction collector ke scope me nahi aaya
-  // tab ye default message bhej dena (fallback ke liye)
+  // Jika interaksi bot tidak termasuk dalam cakupan pengumpul
+  // Kemudian, kirimkan pesan default ini (sebagai cadangan)
   try {
     await interaction.reply({
       content: "❗This button is no longer active or already handled.",
